1c36a33d85dcc2ed6e54baa2cbb64da7069e76a7
Joice Fogaça <joi.fogaca@gmail.com>
2023-12-23 15:39:17 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet("categories")]
+        //[HttpGet("categorias")] Tem a opção de mapear 2x o HttpGet, Exemplo 2 idiomas
+        public IActionResult Get([FromServices] BlogDataContext context) {
+            var categories = context.Categories.ToList();
+            return Ok(categories);
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc; 
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+
+        //health-check
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
