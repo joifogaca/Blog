4f64fe076c67ad2b31b6e9e5355c960205914309
Joice Fogaça <joi.fogaca@gmail.com>
2023-12-29 19:32:27 -0300

    Adcionado métodos HTTP ao CRUD

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,15 +1,86 @@
 ﻿using Blog.Data;
+using Blog.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Controllers
 {
     public class CategoryController : ControllerBase
     {
-        [HttpGet("categories")]
+        [HttpGet("v1/categories")]
         //[HttpGet("categorias")] Tem a opção de mapear 2x o HttpGet, Exemplo 2 idiomas
-        public IActionResult Get([FromServices] BlogDataContext context) {
-            var categories = context.Categories.ToList();
+        public async Task<IActionResult> GetAsync([FromServices] BlogDataContext context) {
+            var categories = await context.Categories.ToListAsync();
             return Ok(categories);
+        }
+
+        [HttpGet("v1/categories/{id:int}")]
+        public async Task<IActionResult> GetByIdAsync(
+            [FromRoute] int id,
+            [FromServices] BlogDataContext context)
+        {
+            var category = await context
+                .Categories
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if(category == null) 
+                return NotFound();
+
+            return Ok(category);
+        }
+
+        [HttpPost("v1/categories")]
+        public async Task<IActionResult> PostAsync(
+            [FromBody] Category model,
+            [FromServices] BlogDataContext context)
+        {
+            await context.Categories.AddAsync(model);
+            await context.SaveChangesAsync();
+
+            return Created($"v1/categories/{model.Id}",model);
+
+        }
+
+        [HttpPut("v1/categories/{id:int}")]
+        public async Task<IActionResult> PostAsync(
+            [FromBody] Category model,
+            [FromRoute] int id,
+            [FromServices] BlogDataContext context)
+        {
+
+            var category = await context
+                .Categories
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            category.Name = model.Name;
+            category.Slug = model.Slug;
+
+            context.Categories.Update(category); //Metódo Update não é assincrono
+            await context.SaveChangesAsync();
+
+            return Ok(model);
+        }
+
+        [HttpDelete("v1/categories/{id:int}")]
+        public async Task<IActionResult> DeleteAsync(
+           [FromRoute] int id,
+           [FromServices] BlogDataContext context)
+        {
+
+            var category = await context
+                .Categories
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            context.Categories.Remove(category); //Metódo Remove não é assincrono
+            await context.SaveChangesAsync();
+
+            return Ok(category);
         }
     }
 }
--- README.md
+++ README.md
@@ -2,4 +2,25 @@
 
 Projeto utilizado no curso balta.io Fundamentos do ASP.NET em .NET 6.0
 
+## Anotações sobre o curso
 
+### Versionamento
+E importante manter o versionamento da API, para seus consumidores saberem quando há mudança nos dados, pode ser interessante manter 2 versões da api ativa, 
+assim o consumidor pode ter um tempo após o lançamento para se preparar para a mudança.
+
+### Async e Await 
+
+Async => O metódo será executado em uma thread separada
+Await => iremos esperar o metódo carregar
+
+Task é um tipo de retorno que em vez de ser o tipo de dado (int, string, List) é um tipo Task que é uma tarefa a ser executada: 
+Task<Categories>
+
+Os métodos do controle também podem ser Assincronos, assim o servidor não irá esperar uma requisição acabar para chamar a próxima
+
+### Métodos HTTP para o CRUD
+
+GET => e usado para retornar informação do recurso identificado na URI. O código de status  200 OK é retornado em caso de sucesso, com os dados desejados no corpo da resposta.
+POST => usado para criar um novo recurso no servidor. Ao realizar a operação servidor com o status 201 CREATED, fornecendo URI do recurso criado.
+PUT => é o método para atualização do recurso ou criar um recurso se não existir. Se bem sucedida devolve o status 200 OK,
+DELETE => método para excluir um recurso. Quando concluido com sucesso, o servidor retorna o código de status No Content 204 caso não encontre o recurso solicitado para remoção.
